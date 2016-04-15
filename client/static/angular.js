ebf765973af43f9217d1c828953eeec861b5bab2
Jose Chery <Jcareergoals@hotmail.com>
2016-04-15 02:20:30 -0400

    created and structured partials for topic and user. added routes for the additional partials.

--- client/static/angular.js
+++ client/static/angular.js
@@ -2,8 +2,10 @@ var board = angular.module('board', ['ngRoute']);
 
 board.config(function($routeProvider){
 	$routeProvider
-		// .when('/', {templateUrl:'./../partials/login.html'})
-		.when('/', {templateUrl:'./../partials/dashboard.html'})
+		.when('/', {templateUrl:'./../partials/login.html'})
+		.when('/dashboard', {templateUrl:'./../partials/dashboard.html'})
+		.when('/topic', {templateUrl:'./../partials/topic.html'})
+		.when('/user', {templateUrl:'./../partials/user.html'})
 		.otherwise({redirectTo:'/'})
 }); 
 
